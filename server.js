1a728ad40c0bad7de71d50bae3143a87d6ffbb88
adiwarsa <adiwarsa03@gmail.com>
2023-08-14 23:38:45 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const db = require("./app/models")
+const cors = require('cors');
+
+app.use(express.json());
+
+app.use(cors());
+
+const mongooseConfig = {
+    useNewUrlParser : true,
+    useUnifiedTopology: true,
+}
+
+db.mongoose.connect(db.url, mongooseConfig)
+.then(()=> console.log("database connected"))
+.catch(err => {
+    console.log(`gagal konel ${err.message}`);
+    process.exit();
+})
+
+require("./app/routes/products.routes")(app);
+require("./app/routes/videos.routes")(app);
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
